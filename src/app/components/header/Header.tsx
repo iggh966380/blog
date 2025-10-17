1bef45be65a77ae3d9157d222c08ee046162280c
21004901TRACY\JasonChang <jason.chang@frog-jump.com>
2025-10-17 17:09:12 +0800

    重構header

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,33 +1,26 @@
 import { Timestamp } from 'next/dist/server/lib/cache-handlers/types';
 import Link from 'next/link';
 import React from 'react';
-import HeaderServer from './HeaderServer';
+import HeaderServer from './NavLinkClient';
 import ModeSwitch from './ModeSwitch';
+import { NavItem } from './models/navItem.interface';
 
-interface NavItem {
-  path: string;
-  created_at: Timestamp;
-  id: number;
-  name: string;
-  parent_id: string;
-  sort: number;
-  updated_at: Timestamp;
-  visible: false;
-}
-
-async function getNavItems(): Promise<NavItem[]> {
+export async function getNavItems(): Promise<NavItem[]> {
   const res = await fetch(`http://localhost:8080/api/getRouter`);
   return res.json();
 }
 
 const Header = async () => {
-  const navItems: NavItem[] = await getNavItems();
-  console.log(navItems);
-
+  const navItems = await getNavItems();
   return (
-    <header className="bg-gray-100 p-8 duration-300 ease-in-out shadow-md color dark:bg-gray-800">
-      <div className="flex items-center justify-between m-auto w-[50%]">
-        <HeaderServer />
+    <header className="bg-gray-100 py-[1rem] duration-300 ease-in-out shadow-md color dark:bg-gray-800">
+      <div className="flex items-center justify-between my-0 mx-auto w-[30%]">
+        <div className="flex items-center">
+          <Link href="/" aria-label="回到首頁" className="pr-[1.5rem]">
+            <h2 className="nav-font">Jason' blog</h2>
+          </Link>
+          <HeaderServer items={navItems} />
+        </div>
         <ModeSwitch />
       </div>
     </header>
